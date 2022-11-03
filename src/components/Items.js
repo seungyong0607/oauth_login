1faf27d95c2a98e46662b5cc17a56b6a1200a449
seungyong0607 <seungyong@sandboxnetwork.net>
2022-11-03 14:52:07 +0900

    이벤트 버블링 간소화

--- src/components/Items.js
+++ src/components/Items.js
@@ -21,17 +21,18 @@ export default class Items extends Component {
   }
 
   setEvent() {
-    this.$target.querySelector('button.addBtn').addEventListener('click', () => {
-      const { items } = this.$state;
-      this.setState({ items: [...items, `item${items.length + 1}`] });
-    });
+    this.$target.addEventListener('click', ({ target })=> {
+      const { items } = this.$state ;
 
-    this.$target.querySelectorAll('.deleteBtn').forEach(deleteBtn =>
-      deleteBtn.addEventListener('click', ({ target }) => {
-        const items = [...this.$state.items];
+      if (target.classList.contains('addBtn')) {
+        this.setState({ items: [ ...items, `item${items.length + 1}` ] });
+      }
+
+      if (target.classList.contains('deleteBtn')) {
         items.splice(target.dataset.index, 1);
         this.setState({ items });
-      })
-    )
+      }
+
+    });
   }
 }
